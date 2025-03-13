1f686f1c6489c8f359ceb60ce99aa012a7164db1
Genji <30670749+ahandsel@users.noreply.github.com>
2025-03-13 16:39:15 +0900

    v1.0.4 Addressed list format issue
    + v1.0.2 - Polish comments

--- docs/tips/google-doc-auto-appending.js
+++ docs/tips/google-doc-auto-appending.js
@@ -1,27 +1,25 @@
 /**
  * Inserts content from a source Google Doc into a target Google Doc after a specified phrase.
  * Versions:
- * + 1.0.1 (2025-03-13): Polished code; added more element-specific handling.
- * 
- * @param {string} sourceDocId - The ID of the source Google Doc.
- * @param {string} targetDocId - The ID of the target Google Doc.
- * @param {string} insertLocation - The phrase after which content will be inserted.
+ * + 1.0.4 (2025-03-13): Addressed teh list format issue
+ * + 1.0.2 (2025-03-13): Polished code; added more element-specific handling.
+ *
+ * @param {string} srcDocId - Source Google Doc's ID.
+ * @param {string} targetDocId - Target Google Doc's ID.
+ * @param {string} insertLocation - Phrase in the target Google Doc after which content will be inserted.
  * @param {string} separator - Optional separator text for clarity.
  */
 
 function insertDocContentAfterPhrase() {
-  const sourceDocId = "SOURCE_GOOGLE_DOC_ID"; // Source Google Doc ID
-  const targetDocId = "TARGET_GOOGLE_DOC_ID"; // Target Google Doc ID
-  const insertLocation = "Insert Below:"; // Target phrase
+  const srcDocId = "SOURCE_GOOGLE_DOC_ID";
+  const targetDocId = "TARGET_GOOGLE_DOC_ID";
+  const insertLocation = "Insert Below:";
   const separator = "----------------------";
 
-  const sourceDoc = DocumentApp.openById(sourceDocId);
-  const targetDoc = DocumentApp.openById(targetDocId);
+  const srcDoc = DocumentApp.openById(srcDocId).getBody();
+  const targetBody = DocumentApp.openById(targetDocId).getBody();
 
-  const sourceBody = sourceDoc.getBody();
-  const targetBody = targetDoc.getBody();
-
-  // Find the paragraph insertIndex that contains the phrase using Array.findIndex
+  // Find the paragraph index that contains the insert location phrase.
   const targetParagraphs = targetBody.getParagraphs();
   const targetIndex = targetParagraphs.findIndex((paragraph) =>
     paragraph.getText().includes(insertLocation)
@@ -37,36 +35,42 @@ function insertDocContentAfterPhrase() {
   targetBody.insertParagraph(insertIndex, separator);
   insertIndex++;
 
-  // Build an array of copied source elements using Array.from
-  const numChildren = sourceBody.getNumChildren();
-  const sourceElements = Array.from({ length: numChildren }, (_, j) =>
-    sourceBody.getChild(j).copy()
+  // Build an array of source elements using Array.from.
+  const srcElements = Array.from({ length: srcDoc.getNumChildren() }, (_, j) =>
+    srcDoc.getChild(j)
   );
 
-  // Insert each copied element into the target document with the appropriate method
-  sourceElements.forEach((element) => {
-    const type = element.getType();
-    switch (type) {
+  // Helper function to insert elements based on type.
+  const insertElement = (element, index) => {
+    switch (element.getType()) {
       case DocumentApp.ElementType.INLINE_IMAGE:
-        targetBody.insertImage(insertIndex, element);
+        targetBody.insertImage(index, element.copy());
         break;
       case DocumentApp.ElementType.PAGE_BREAK:
-        targetBody.insertPageBreak(insertIndex, element);
+        targetBody.insertPageBreak(index, element.copy());
         break;
       case DocumentApp.ElementType.PARAGRAPH:
-        targetBody.insertParagraph(insertIndex, element);
+        targetBody.insertParagraph(index, element.copy());
         break;
       case DocumentApp.ElementType.TABLE:
-        targetBody.insertTable(insertIndex, element);
+        targetBody.insertTable(index, element.copy());
         break;
-      case DocumentApp.ElementType.LIST_ITEM:
-        targetBody.insertListItem(insertIndex, element);
+      case DocumentApp.ElementType.LIST_ITEM: {
+        const newListItem = targetBody.insertListItem(index, element.getText());
+        newListItem.setAttributes(element.getAttributes());
+        newListItem.setGlyphType(element.getGlyphType());
         break;
+      }
       default:
-        // For any unhandled element types, default to inserting as a paragraph.
-        targetBody.insertParagraph(insertIndex, element);
+        console.log("Element type not supported: " + element.getType());
+        targetBody.insertParagraph(index, element.copy()); // Insert as a paragraph by default.
         break;
     }
+  };
+
+  // Insert each source element into the target document.
+  srcElements.forEach((element) => {
+    insertElement(element, insertIndex);
     insertIndex++;
   });
 
