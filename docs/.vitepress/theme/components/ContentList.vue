5d898776be0b8751143e5b6c9884a2e0aa1dbe75
Genji <30670749+ahandsel@users.noreply.github.com>
2025-03-14 22:59:22 +0900

    test

---
+++ docs/.vitepress/theme/components/ContentList.vue
@@ -0,0 +1,21 @@
+<script setup>
+const pages = import.meta.glob('/docs/tips/*.md', { eager: true });
+
+const pageList = Object.keys(pages).map((path) => {
+  return {
+    title: pages[path].frontmatter.title || path.split('/').pop().replace('.md', ''),
+    link: path.replace('/docs', '').replace('.md', ''),
+  };
+});
+</script>
+
+<template>
+  <div>
+    <h2>Articles</h2>
+    <ul>
+      <li v-for="page in pageList" :key="page.link">
+        <a :href="page.link">{{ page.title }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
