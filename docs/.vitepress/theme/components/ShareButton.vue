74db1bf6e7c32ca594ef19d3cd0bbe921144918d
Genji <30670749+ahandsel@users.noreply.github.com>
2025-03-21 23:58:19 +0900

    Add Share button to copy page link in VitePress site
    Add "Share" button to copy page link in VitePress site.
    
    * **Add ShareButton Component**
      - Create `ShareButton.vue` in `docs/.vitepress/theme/components/`
      - Add button to copy current page URL to clipboard
      - Show confirmation message when URL is copied
    
    * **Update VitePress Theme**
      - Import and register `ShareButton.vue` in `docs/.vitepress/theme/index.ts`
      - Include `ShareButton` in `layout-bottom` slot of default theme layout
    
    * **Modify VitePress Configuration**
      - Update `docs/.vitepress/config.mts` to add "Share" button to footer section
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/ahandsel/tokyo-geek?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ docs/.vitepress/theme/components/ShareButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="copyLink">Share</button>
+    <div v-if="showConfirmation" class="confirmation">Link copied!</div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const showConfirmation = ref(false)
+
+function copyLink() {
+  const url = window.location.href
+  navigator.clipboard.writeText(url).then(() => {
+    showConfirmation.value = true
+    setTimeout(() => {
+      showConfirmation.value = false
+    }, 2000)
+  })
+}
+</script>
+
+<style>
+.confirmation {
+  color: green;
+  font-size: 0.9em;
+  margin-top: 5px;
+}
+</style>
